cbafdeda1b07654d708d317e39eba07a3d0f1401
root <gerald.schade@gmx.de>
2016-08-21 19:01:21 +0200

    Version 0.40506

--- autofax.cpp
+++ autofax.cpp
@@ -5442,7 +5442,7 @@ int paramcl::pruefcapi()
         if (system!=sus)
           linst.doggfinst("capiutils",obverb+1,oblog);
         if (system==fed) {
-          systemrueck("tar xvf jpegsrc.v9b.tar.gz >/dev/null && cd jpeg-9b && ./configure && make && make install "
+          systemrueck("which sfftobmp || tar xvf jpegsrc.v9b.tar.gz >/dev/null && cd jpeg-9b && ./configure && make && make install "
                       " && yum -y install boost "
                       " && { grep '/usr/local/lib' /etc/ld.so.conf || { echo '/usr/local/lib' >> /etc/ld.so.conf; ldconfig; } } && cd .. "
                       " && unzip sfftobmp_3_1_src.zip >/dev/null && cd sfftobmp3.1 "
