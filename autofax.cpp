35bb1ad41466ea9366d89207ef9a3bc9dcfd57b0
root <gerald.schade@gmx.de>
2016-08-29 01:08:57 +0200

    Version 0.40647

--- autofax.cpp
+++ autofax.cpp
@@ -6635,7 +6635,6 @@ int main(int argc, char** argv)
     } else {
 
       // hier stehen obcapi und obhyla fest
-      // exit(0);
       pm.faxealle();
       // Dateien in Spool-Tabelle nach inzwischen verarbeiteten durchsuchen, Datenbank- und Dateieintraege korrigieren 
       pm.untersuchespool();
