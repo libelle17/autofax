9dca78ccf50583a0ca9d7d5203e473d80e677d5a
root <gerald.schade@gmx.de>
2016-09-04 21:23:22 +0200

    Version 0.40752

--- autofax.cpp
+++ autofax.cpp
@@ -5178,6 +5178,7 @@ int paramcl::pruefhyla()
         linst.doinst("tiff",obverb+1,oblog,"tiff2ps");
         linst.doinst("tiff",obverb+1,oblog,"fax2ps");
         linst.doinst("sendmail",obverb+1,oblog,"sendmail", wahr);
+        if (obverb) Log(blaus+"hyinstart: "+schwarz+ltoan(hyinstart),obverb,oblog);
         if (hyinstart==hysrc) {
           if (1) {
             systemrueck("sudo wget -O hylafax+ https://sourceforge.net/projects/hylafax/files/latest",obverb,oblog);
