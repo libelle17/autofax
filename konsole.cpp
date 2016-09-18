489d70ff79b98284d1ad8f0754bcfa06ef5e12d7
root <gerald.schade@gmx.de>
2016-09-18 19:27:13 +0200

    Version 0.40877

--- konsole.cpp
+++ konsole.cpp
@@ -2274,8 +2274,7 @@ int servc::machfit(int obverb,int oblog, binaer nureinmal)
         systemrueck("sudo setenforce 1",obverb,oblog);
         linst.doinst("policycoreutils",obverb+1,oblog,"semodule");
         systemrueck("sudo semodule -i \""+sname+"_selocal.pp\"",obverb,oblog);
-        exit(100);
-      }
+      }  // if (obse)
       //       KLZ
       //      KLZ
     } // if (serviceda && !servicelaeuft) 
