bab4ae22293b7c719acd913cf06327088e2262b7
root <gerald.schade@gmx.de>
2016-11-03 15:11:24 +0100

    Version 0.41396

--- install.sh
+++ install.sh
@@ -1,3 +1,5 @@
 #!/bin/sh
+{ rpm -q g++ >/dev/null 2>&1 || dpkg -s g++ >/dev/null 2>&1;} || sudo zypper -n --gpg-auto-import-keys in g++ || { which apt-get && sudo apt-get -y --force-yes install g++ ;} || { which dnf && sudo dnf -y install g++; } || { which yum && sudo yum -y install g++; }
 { rpm -q make >/dev/null 2>&1 || dpkg -s make >/dev/null 2>&1;} || sudo zypper -n --gpg-auto-import-keys in make || { which apt-get && sudo apt-get --assume-yes install make ;} || { which dnf && sudo dnf -y install make; } || { which yum && sudo yum -y install make; } && make && make install
+
 
