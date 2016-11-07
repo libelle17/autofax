af38231206a2134784e9e68d22fc66e63743f682
root <gerald.schade@gmx.de>
2016-11-07 14:26:15 +0100

    Version 0.41441

--- autofax.cpp
+++ autofax.cpp
@@ -4166,12 +4166,18 @@ int paramcl::pruefocr()
 						double vers=0;
 						if (urueck.size()) vers=atol(urueck[0].c_str());
 						if (!urueck.size()||vers<6.1) {
+// sudo rpm -Uvh http://download1.rpmfusion.org/free/fedora/rpmfusion-free-release-stable.noarch.rpm http://download1.rpmfusion.org/nonfree/fedora/rpmfusion-nonfree-release-stable.noarch.rpm
+// sudo dnf install ffmpeg
+// sudo dnf install ffmpeg-devel
+// sudo dnf install ffmpeg-compat
+// sudo PKG_CONFIG_PATH=/usr/lib64/ffmpeg-compat/pkgconfig pkg-config --cflags libavcodec
 							if (pruefipr()==apt||pruefipr()==dnf||pruefipr()==yum) linst.doggfinst("libavformat-dev",obverb+1,oblog);
 							holvongithub("unpaper_copy");
 							if (vers) systemrueck("sudo rm $(which unpaper) && hash -r",obverb,oblog);
 							kompiliere("unpaper_copy",s_gz);
 						} // 						if (!urueck.size()||vers<6.1)
-            systemrueck("sh -c 'cd \""+instverz+vtz+proj+"\" && sudo -H pip3 install image PyPDF2 ruffus reportlab M2Crypto cryptography cffi ocrmypdf'",
+						// in fedora pip statt pip3
+            systemrueck("sh -c 'cd \""+instverz+vtz+proj+"\" && sudo -H $(which pip3||which pip) install image PyPDF2 ruffus reportlab M2Crypto cryptography cffi ocrmypdf'",
 						            obverb,oblog);
             linst.doinst("unpaper",obverb,oblog);
           } //    if (!kompilbase(was,endg))
