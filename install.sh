d225b7bca3430e321628841472038a70f36d9bb9
root <gerald.schade@gmx.de>
2016-12-11 13:29:14 +0100

    Version 0.41624

--- install.sh
+++ install.sh
@@ -1,5 +1,6 @@
 #!/bin/sh
 rot="\033[1;31m"
+blau="\033[1;34m"
 reset="\033[0m"
 P=autofax
 aPWD=`pwd`
@@ -16,22 +17,6 @@ getIPR() {
  { which dpkg >/dev/null 2>&1 && SPR="dpkg -s ";}
 }
 
-fuellTU() {
-TU="{ rpm -q make >/dev/null 2>&1 ||dpkg -s make >/dev/null 2>&1;} ||
-    { which zypper >/dev/null 2>&1 &&
-      { sudo zypper -n --gpg-auto-import-keys in make >/dev/null 2>&1;true;} ||
-    { which apt-get >/dev/null 2>&1 &&
-      { sudo apt-get --assume-yes install make >/dev/null 2>&1;true;} ||
-    { which dnf >/dev/null 2>&1 &&
-      { sudo dnf -y install make >/dev/null 2>&1;true;} ||
-    { which yum >/dev/null 2>&1 &&
-      { sudo yum -y install make >/dev/null 2>&1;true;} } } } } && 
-    { [ $nPWD = $P -a -f Makefile ]&& 
-      { cd '$aPWD' && make && make install;true;} ||
-      { echo $nPWD; echo $P;} ||
-      { T=$P.tar.gz; cd ~; wget https://github.com/libelle17/$P/archive/master.tar.gz -O ($T) && tar xpvf $T && rm -f $T && { for i in "$VORIGE"; do j=\"$((i+1))\"; case $i in ""|*[!0-9]*);; *) mv ${P}_$i ${P}_$j; esac; done; test -e $P && mv $P ${P}_1; mv $P-master $P && cd $P; } }"
-}
-#erg=1; which rpm &&{ rpm -q make >/dev/null 2>&1;erg=$?;true;} || which dpkg &&{ dpkg -s make >/dev/null 2>&1;erg=$?;true;}; (exit $erg)|| 
 SUG="root\| sudo\| wheel\| admin";
 getIPR;
 which sudo >/dev/null && id -Gn $USER|grep -qw "$SUG"||{ 
@@ -60,18 +45,18 @@ $SPR make >/dev/null || exit
  tar xpvf $T && 
  rm -f $T && {
 	VORIGE=$(ls -d ~/${P}_* 2>/dev/null | cut -d"_" -f2 | sort -nr);
-  echo -e Vorige: ${rot}$VORIGE${reset};
-	for i in "$VORIGE"; do 
-    j=$((i+1)); 
-    case $i in ""|*[!0-9]*);; *) mv ${P}_$i ${P}_$j; esac; 
+	for i in $VORIGE; do 
+    case $i in ""|*[!0-9]*);; *) 
+		j=$((i+1)); 
+		echo mv ${P}_$i ${P}_$j; 
+		mv ${P}_$i ${P}_$j; 
+		esac; 
   done; 
   test -e $P && mv $P ${P}_1; 
   mv $P-master $P && 
   cd $P &&
   make &&
-  sudo make install;
-  echo Ergebniscode: $?
+  sudo make install && farbe=$blau || farbe=$rot
+  echo -e Ergebniscode: ${farbe}$?${reset}
  }
 }
-echo Hier bin ich noch
-
