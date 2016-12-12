a73deb4a611ff8562504b7972e8b9cf9195328c8
root <gerald.schade@gmx.de>
2016-12-12 21:36:49 +0100

    Version 0.41674

--- install.sh
+++ install.sh
@@ -2,9 +2,9 @@
 P=autofax
 HOSTER=github.com
 ACC=libelle17
-rot="\033[1;31m"
-blau="\033[1;34m"
-reset="\033[0m"
+rot="\e[1;31m"
+blau="\e[1;34m"
+reset="\e[0m"
 aPWD=`pwd`
 nPWD=${PWD##*/}
 IPR="nix"
@@ -26,9 +26,9 @@ SUG="root\|sudo\|wheel\|admin";
 # hier geht's los
 getIPR;
 { which sudo >/dev/null && id -Gzn $USER|grep -qw "$SUG";}||{ 
-	echo -e "Must allow '$blau$USER$reset' to call '${blau}sudo$reset'. Please enter ${blau}root$reset's password if asked:\nMuss '$blau$USER$reset' den Aufruf von '${blau}sudo$reset' ermoeglichen. Bitte geben Sie bei der Frage das Passwort von '${blau}root$reset' ein:";
+	printf "Must allow '$blau$USER$reset' to call '${blau}sudo$reset'. Please enter ${blau}root$reset's password if asked:\nMuss '$blau$USER$reset' den Aufruf von '${blau}sudo$reset' ermoeglichen. Bitte geben Sie bei der Frage das Passwort von '${blau}root$reset' ein:\n";
 	su -c "$IPR sudo; usermod -aG $(cut -d: -f1 /etc/group|grep "$SUG"|head -n 1) "$USER";"||exit
-	echo -e "Please log out and in again, change to the directory '$blau$PWD$reset' and then call this script again!\nBitte loggen Sie sich jetzt aus und nochmal ein, wechseln Sie nach '$blau$PWD$reset' und rufen Sie dieses Script dann nochmal auf!";
+	printf "Please log out and in again, change to the directory '$blau$PWD$reset' and then call this script again!\nBitte loggen Sie sich jetzt aus und nochmal ein, wechseln Sie nach '$blau$PWD$reset' und rufen Sie dieses Script dann nochmal auf!\n";
 	exit;
 }
 $SPR make >/dev/null 2>&1 ||{
@@ -38,10 +38,10 @@ $SPR make >/dev/null 2>&1 ||{
 $SPR make >/dev/null || exit
 # wenn schon im Verzeichnis $P und wenn es dort einige notwendige Dateien gibt, dann nur kompilieren und installieren
 [ $nPWD = $P -a -f Makefile -a -f $P.cpp ]&&{
-	echo -e ${rot}Installing/ Installiere ... $reset
+	printf "${rot}Installing/ Installiere ... $reset\n"
 	true;
 }||{
-	echo -e ${rot}Downloading/ Lade runter ... $reset
+	printf "${rot}Downloading/ Lade runter ... $reset\n"
 	T=$P.tar.gz;
 	cd ~; 
 	wget https://$HOSTER/$ACC/$P/archive/master.tar.gz -O "$T" && 
@@ -63,4 +63,4 @@ $SPR make >/dev/null || exit
 make &&
 sudo make install; erg=$?;
 [ $erg = 0 ] && farbe=$blau || farbe=$rot;
-echo -e Result code/ Ergebniscode: ${farbe}$erg${reset}
+printf "Result code/ Ergebniscode: ${farbe}$erg${reset}\n"
