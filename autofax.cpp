fb1160d4941312a1413fc71aef39f3c5beb6086b
root <gerald.schade@gmx.de>
2016-12-17 13:09:54 +0100

    Version 0.41698

--- autofax.cpp
+++ autofax.cpp
@@ -4905,7 +4905,8 @@ void paramcl::untersuchespool(uchar mitupd) // faxart 0=capi, 1=hyla
           int obsfehlt=-1;
           /*fsf.*/
           setzhylastat(&fsf, &protdakt, &hyla_uverz_nr, 0, &obsfehlt, 1, oblog);
-          fsf.hylaausgeb(&ausg, this, obsfehlt, 0, obverb, 0, oblog);
+					caus <<gruen<<"fsf.hylastat: "<<schwarz<<(int)fsf.hylastat<<endl;
+					fsf.hylaausgeb(&ausg, this, obsfehlt, 0, obverb, 0, oblog);
 //          if (!obsfehlt) KLA // Protokolldatei vorhanden 12.10.16 sollte jetzt auch mit xferfax gehen
             if (mitupd) {
               RS rupd(My); 
