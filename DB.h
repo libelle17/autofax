2b56d04986b88f51bbf0b58878e67f2f6253d7bc
root <gerald.schade@gmx.de>
2016-12-19 00:55:08 +0100

    Version 0.41743

--- DB.h
+++ DB.h
@@ -31,7 +31,6 @@ using namespace std;
 #ifdef _MSC_VER
 //#pragma comment(lib, "libmysql.lib")
 #endif
-#define CLIENT_MULTI_STATEMENTS (1UL << 16) /* Enable/disable multi-stmt support */
 
 enum Txdb_ 
 {
