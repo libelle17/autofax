dc9faa0f0d213fe01808fb8e0394b9af4d3dff87
root <gerald.schade@gmx.de>
2016-12-19 01:12:46 +0100

    Version 0.41746

--- autofax.cpp
+++ autofax.cpp
@@ -5218,11 +5218,11 @@ void paramcl::sammlefertigehyla(vector<fsfcl> *fsfvp)
 		inse[inse.size()-1]=';';
 		auswm[auswm.size()-1]=')';
 		insm.erase(insm.length()-1);
-		/*
-		mysql_set_server_option(My->conn,MYSQL_OPTION_MULTI_STATEMENTS_ON);
-		RS vgl(My,"DROP TABLE IF EXISTS tmp; CREATE TABLE tmp(i INT KEY); INSERT INTO tmp VALUES "+inse,ZDB);
-		mysql_set_server_option(My->conn,MYSQL_OPTION_MULTI_STATEMENTS_OFF);
-		*/
+//		mysql_set_server_option(My->conn,MYSQL_OPTION_MULTI_STATEMENTS_ON);
+		RS vgl1(My,"DROP TABLE IF EXISTS tmp",ZDB);
+		RS vgl2(My,"CREATE TABLE tmp(i INT KEY);",ZDB);
+		RS vgl3(My,"INSERT INTO tmp VALUES "+inse,ZDB);
+//		mysql_set_server_option(My->conn,MYSQL_OPTION_MULTI_STATEMENTS_OFF);
 		char ***cerg;
 		RS rs1(My,"SELECT submid FROM `"+touta+"` WHERE erfolg=0 AND submid IN "+auswe,ZDB); // "` where concat('q',hylanr)='"+rueck[i]+"'",ZDB);
 		size_t cergz=0;
