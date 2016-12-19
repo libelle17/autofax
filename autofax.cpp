180d6743fb8849f0fc7f00e5e7de0252e48c7fb1
root <gerald.schade@gmx.de>
2016-12-19 03:02:07 +0100

    Version 0.41756

--- autofax.cpp
+++ autofax.cpp
@@ -5207,6 +5207,7 @@ void paramcl::sammlefertigehyla(vector<fsfcl> *fsfvp)
 			vector<string> tok; 
 			aufSplit(&tok,&qrueck[i],'\t');
 			if (tok.size()>5) {
+			  caus<<tok[0]<<'|'<<tok[1]<<'|'<<tok[2]<<'|'<<tok[3]<<'|'<<tok[4]<<'|'<<tok[5]<<'|'<<tok[6]<<endl;
 				uchar erfolg=0;
 				if (qrueck[i].substr(0,4)=="SEND") {
 					if (tok[5]=="\"\"") erfolg=1;
