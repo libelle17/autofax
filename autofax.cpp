faeb87ee4edb02507678a2b36920c3a3b6dc5577
root <gerald.schade@gmx.de>
2017-01-24 23:50:50 +0100

    Version 0.42144

--- autofax.cpp
+++ autofax.cpp
@@ -2948,7 +2948,9 @@ void paramcl::rueckfragen()
       systemrueck(cmd,obverb,oblog,&benutzer);
 			if (benutzer.size()>1) for(size_t i=benutzer.size();i;) {
 				--i;
+				  caus<<gruen<<"i: "<<i<<", Benutzer vor Pruefung: "<<benutzer[i]<<endl;
 				if (benutzer[i]=="syslog"||benutzer[i]=="ntp") {
+				  caus<<gruen<<"Benutzer nach Pruefung: "<<benutzer[i]<<endl;
 					benutzer.erase(benutzer.begin()+i);
 				}
 			} // for(size_t i=benutzer.size();i;)
