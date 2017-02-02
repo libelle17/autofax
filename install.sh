a90ec78eb6e5d90a77083610df9d536243cac9b0
root <gerald.schade@gmx.de>
2017-02-02 20:03:35 +0100

    Version 0.42231

--- install.sh
+++ install.sh
@@ -9,36 +9,7 @@ reset="\033[0m"
 aPWD=`pwd`
 nPWD=${PWD##*/}
 
-IPR="nix"
-UPR="nix"
-SPR="nix"
-UN="uninstall.sh"
-pgroff="groff groff-base"
-dev=devel
-libmc=libmysqlclient
-fed=0
-REPOS=
-urepo=
-CCInst=gcc6-c++
-
-# Installationsprogramm ermitteln
-getIPR() {
-	{ which zypper  >/dev/null 2>&1 &&{ 
-		Z=zypper;g=--gpg-auto-import-keys;IPR="$Z -n $g in ";IP_R="$Z $g in ";UPR="sudo zypper rm -u ";pgroff=groff;
-  REPOS="sudo zypper lr|grep 'g++\|devel_gcc'\$(OR)||sudo zypper ar http://download.opensuse.org/repositories/devel:/gcc/\`cat /etc/*-release|grep ^NAME= |cut -d'\"' -f2|sed 's/ /_/'\`_\`cat /etc/*-release|grep ^VERSION_ID= |cut -d'\"' -f2\`/devel:gcc.repo;";
-  urepo="sudo zypper lr|grep \\\"g++\\\\\|devel_gcc\\\"\$(OR) && sudo zypper rr devel_gcc;";
-  COMP="gcc gcc-c++ \$(CCInst)";
-	} }||
-	{ which apt-get >/dev/null 2>&1 &&{ IPR="apt-get --assume-yes install ";
-	                                    UPR="sudo apt-get purge --auto-remove ";
-																			dev=devel;
-																			COMP:="build-essential linux-headers-\`uname -r\`";} }||
-	{ which dnf     >/dev/null 2>&1 &&{ fed=1;IPR="dnf -y install ";UPR="sudo dnf remove ";} }||
-	{ which yum     >/dev/null 2>&1 &&{ fed=1;IPR="yum -y install ";UPR="sudo yum remove ";} }
-	[ $fed = 1 ] &&{ libmc=mysql;COMP="make automake gcc-c++ kernel-devel";}
-	{ which rpm >/dev/null 2>&1 && SPR="rpm -q ";}||
-	{ which dpkg >/dev/null 2>&1 && SPR="dpkg -s ";}
-}
+. ./hdistr.sh
 
 exportvars() {
 	rm -f vars
