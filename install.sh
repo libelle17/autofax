405d0c370aa91205a6791527bd863febd890560b
root <gerald.schade@gmx.de>
2017-02-26 20:47:04 +0100

    Version 0.42702

--- install.sh
+++ install.sh
@@ -79,8 +79,7 @@ getIPR;
 	printf "Bitte geben Sie bei den beiden Fragen das Passwort von '${blau}root$reset' ein:\n";
 #	su -c "$IPR sudo;";grep -q \"sudo\" $AUNF||printf \"${UPR}sudo\necho \\\"${UPR}sudo\\\"\n\">>$AUNF;
 	echo "AUNF: " $AUNF
-	echo "UPR sudo: " ${UPR}sudo
-	test -f $AUNF&&grep -q \"${UPR}sudo\" $AUNF||printf "${UPR}sudo\nprintf \"\$blau%%s\$reset\\\n\" \"${UPR}sudo\"\n">>$AUNF;
+	test -f $AUNF&&grep -q \"[^;] sudo\" $AUNF||printf "${UPR}sudo\nprintf \"\$blau%%s\$reset\\\n\" \"${UPR}sudo\"\n">>$AUNF;
 	su -c "$IdPR sudo;";
 	su -c "usermod -aG $(cut -d: -f1 /etc/group|grep -w "$SUG"|tail -n1) "$USER";"||exit
 	printf "Please log out and in again, change to the directory '$blau$PWD$reset' and then call '${blau}sh $0$reset'!\n"
