23a3354e6728d51d8f490f61938e43575d203953
root <gerald.schade@gmx.de>
2017-05-01 13:31:15 +0200

    Version 0.43274

--- kons.cpp
+++ kons.cpp
@@ -2712,15 +2712,15 @@ int linst_cl::doinst(const string& prog,int obverb/*=0*/,int oblog/*=0*/,const s
 			} // 			for(unsigned i=srueck.size();i;)
 
 		// s. ausricht() in configure
-			mdatei uniff("inst_log",ios::app,0);
+			mdatei uniff(instvz+"/inst_log",ios::app,0);
 			if (uniff.is_open()) {
 				uniff<<endl<<"Rueckmeldung zu: '"<<bef<<"':"<<endl;
-				for(unsigned i=srueck.size();i;) {
+				for(unsigned i=srueck.size();i++;) {
 					uniff<<srueck[i]<<endl;
 				}
 				uniff<<"ustring: '"<<ustring<<"'"<<endl;
 				uniff<<"ustring: '"<<string_to_hex(ustring)<<"'"<<endl;
-			}
+			} // 			if (uniff.is_open())
 			size_t p1,p2;
 			caus<<violett<<"ustring vor Pruefung: "<<rot<<ustring<<schwarz<<endl;
 			caus<<violett<<"ustring vor Pruefung: "<<rot<<string_to_hex(ustring)<<schwarz<<endl;
@@ -2735,7 +2735,7 @@ int linst_cl::doinst(const string& prog,int obverb/*=0*/,int oblog/*=0*/,const s
 			if (!ustring.empty()) {
 				Log(Txk[T_Ins_Deinstallationsprogramm_wird_eingetragen]+violetts+udpr+ustring+schwarz,obverb,oblog);
 				anfgw(unindt,udpr,ustring,bef,obverb,oblog);
-			}
+			} // 			if (!ustring.empty())
 
 			/*
 			if (0) {
