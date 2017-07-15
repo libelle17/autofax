71edc6ed0c79c82a1aade094c324c80f9c9135d7
root <gerald.schade@gmx.de>
2017-07-15 11:06:45 +0200

    Version 0.4392

---
+++ DB.h
@@ -0,0 +1,389 @@
+#ifdef _MSC_VER
+#define _CRT_SECURE_NO_WARNINGS
+#endif
+//#pragma once
+// Visual c++:
+// Project->Properties->C++->general->additional include directories
+// C:\Programme\MySQL\Connector C 6.0.2\include
+// mingw: 
+// -I"C:\Programme\MySQL\Connector C 6.0.2\include"
+#ifndef DB_H_DRIN
+#define DB_H_DRIN
+// #include <vector>
+// #include <string>
+// #include <iostream> // fuer cout ggf.
+#include <sstream> // stringstream, basic_stringstream
+// Project->Properties->C/C++ ->General->Additional Include Directories->"$(ProjectDir)\..\..\..\MySQL Connector C 6.0.2\include"
+#include <mysql.h> 
+// sudo dnf install postgresql-devel
+//#define mitpostgres
+#ifdef mitpostgres 
+#include <libpq-fe.h> // PGconn
+#endif
+// G.Schade 26.1.14: Den Rest aus der Datei braucht man scheinbar nicht; sonst Konflikt mit bitset unter -std=gnu++11
+#ifndef HAVE_UCHAR
+typedef unsigned char	uchar;	/* Short for unsigned char */
+#endif
+// #include "/usr/include/mysql/my_global.h"
+// #include <my_global.h> 
+using namespace std;
+#ifdef linux
+// #include <string.h>
+#endif
+// Project->Properties->Linker->general->(mitte)->additional library directories:
+// C:\Programme\MySQL\Connector C 6.0.2\lib\opt
+// weiterhin: libmysql.dll ins Verzeichnis der Exe stellen
+#ifdef _MSC_VER
+//#pragma comment(lib, "libmysql.lib")
+#endif
+
+enum Txdb_ 
+{
+  T_DB_wird_initialisiert,
+  T_Fehler_db,
+  T_beim_Initialisieren_von_MySQL,
+  T_Fehler_dp,
+  T_bei_Befehl,
+  T_Versuche_mysql_zu_starten,
+  T_MySQL_erfolgreich_gestartet,
+  T_Versuche_Datenbank,
+  T_zu_erstellen,
+  T_Fehler_beim_Verbinden,
+  T_Erfolg_beim_Initialisieren_der_Verbindung_zu_mysql,
+	T_MySQL_Passwort,
+	T_fuer_Benutzer,
+	T_wird_benoetigt_fuer_Befehl,
+  T_ist_leer_Wollen_Sie_eines_festlegen,
+  T_j,
+  T_Bitte_geben_Sie_ein_MySQL_Passwort_fuer_Benutzer_root_ein,
+  T_Fuehre_aus_db,
+  T_falsche_Fehlernr,
+  T_bei_der_Abfrage_der_Spaltenlaenge_bei_Tabelle,
+  T_und_Feld,
+  T_mit,
+  T_bei_Abfrage,
+  T_Fehler_beim_Pruefen_auf_Vorhandensein_des_Datensatzes,
+  T_Datenbank_nicht_zu_oeffnen,
+  T_Erweitere_Feld,
+  T_von,
+  T_auf,
+  T_Aendere_Feld,
+  T_Pruefe_Tabelle,
+  T_Lesespalten,
+	T_Vor_restart,
+	T_Versuch_Nr,
+	T_bei_sql_Befehl,
+	T_PostgreSQL_musste_neu_eingerichtet_werden,
+	T_Bitte_geben_Sie_ein_Passwort_fuer_Benutzer_postgres_ein,
+	T_Welches_Passwort_soll_der_Benutzer_postgres_haben,
+	T_Ende_Gelaende,
+	T_Verbindung_zu,
+	T_gelungen,
+	T_prueffunc,
+	T_dbMAX,
+};
+
+// extern class Txdbcl Txd;
+extern const char *DB_T[T_dbMAX+1][SprachZahl];
+extern class TxB Txd;
+/*//
+class Txdbcl: public TxB
+{
+  public:
+    static const char *TextC[][SprachZahl];
+    Txdbcl();
+};
+*/
+svec holdbaussql(string sql);
+
+enum DBSTyp {MySQL, Postgres};
+
+class sqlft: public string 
+{
+  private:
+    ////	char dbuf[21];
+    string *ersetze(const char* alt, const char* neu);
+    string *sersetze( string *src, string const& target, string const& repl);
+    void druckeein(DBSTyp eDBS, tm *zt);
+  public:
+    ////	string feld;
+    sqlft(DBSTyp eDBS, const string& vwert);
+    sqlft(DBSTyp eDBS, const string* vwert);
+    sqlft(DBSTyp eDBS, char* vwert,char* zs);
+    sqlft(DBSTyp eDBS, char* vwert,bool obzahl=0);
+    sqlft(DBSTyp eDBS, time_t *zt);
+    sqlft(DBSTyp eDBS, tm *zt);
+    sqlft(DBSTyp eDBS, char c);
+    sqlft(DBSTyp eDBS, uchar c);
+    sqlft(DBSTyp eDBS, int i);
+    sqlft(DBSTyp eDBS, long int i);
+    sqlft(DBSTyp eDBS, unsigned int i);
+    sqlft(DBSTyp eDBS, unsigned long int i);
+    sqlft(DBSTyp eDBS, long long int i);
+};
+
+template<typename T, size_t N> T * end(T (&ra)[N]) 
+{
+  return ra + N;
+}
+
+class instyp 
+{
+ public:
+    const string feld;
+    string wert;
+    unsigned char obkeinwert; // bei update wird <wert> nicht als Wert, sondern ohne Anf'z.(z.B.als Feld) verwendet (z.B. update xy set altdatum = datum)
+  private:
+    ////	char dbuf[21];
+    inline string ersetze(const char *u, const char* alt, const char* neu);
+    inline string *sersetze( string *src, string const& target, string const& repl);
+  public:
+    /*1*/template <typename tC> explicit instyp (DBSTyp eDBS, char* const feld, tC vwert): feld(feld) {
+      wert=sqlft(eDBS,vwert);
+      obkeinwert=0;
+    }
+    /*2*/template <typename tC> explicit instyp (DBSTyp eDBS, const char* feld, tC vwert):feld(feld) {
+      wert=sqlft(eDBS,vwert);
+      obkeinwert=0;
+    }
+////    void init(){feld="";wert="";obkeinwert=0;}
+
+    /*3*/instyp (DBSTyp eDBS, char* feld, char *vwert):feld(feld) {
+      wert=sqlft(eDBS,vwert,false);
+      obkeinwert=0;
+    }
+    /*4*/instyp (DBSTyp eDBS, char* feld, char *vwert,char* zs):feld(feld) {
+      wert=sqlft(eDBS,vwert,zs);
+      obkeinwert=0;
+    }
+    /*5*/instyp (DBSTyp eDBS, char* feld, char *vwert,bool obzahl):feld(feld) {
+      wert=sqlft(eDBS,vwert,obzahl);
+      obkeinwert=0;
+    }
+    /*6*/instyp (DBSTyp eDBS, const char* feld, const char *vwert,unsigned char vobkeinwert):feld(feld) {
+      wert=vwert;
+      obkeinwert=vobkeinwert;
+    }
+};
+// delimiter value begin
+inline char dvb(DBSTyp DBS) 
+{
+  switch(DBS) {
+    case MySQL: return '\'';
+    case Postgres: return '\"';
+    default: return '"';
+  }
+}
+// delimiter value end
+inline char dve(DBSTyp DBS) 
+{
+  switch(DBS) {
+    case MySQL: return '\'';
+    case Postgres: return '\"';
+    default: return '"';
+  }
+}
+
+// delimiter name begin
+inline char dnb(DBSTyp DBS) 
+{
+  switch(DBS) {
+    case MySQL: return '`';
+    case Postgres: return '\"';
+    default: return '"';
+  }
+}
+// delimiter name end
+inline char dne(DBSTyp DBS) 
+{
+  switch(DBS) {
+    case MySQL: return '`';
+    case Postgres: return '\"';
+    default: return '"';
+  }
+}
+
+class Feld 
+{
+  public:
+    const string name;
+    const string typ;
+    string lenge;
+    const string prec;
+    string comment;
+    bool obind;
+    bool obauto;
+    bool nnull;
+    string defa;
+		bool unsig;
+		Feld();
+    Feld(const string& name, string typ="", const string& lenge="", const string& prec="", 
+         const string& comment="", bool obind=0, bool obauto=0, bool nnull=0, const string& defa="", bool unsig=0);
+//		Feld(Feld const& copy);
+};
+
+class Index 
+{
+	public:
+		const string name;
+    int feldzahl;
+    Feld *felder;
+    Index(const string& vname, Feld *vfelder, int vfeldzahl);
+};
+
+class Tabelle 
+{
+  public:
+    const string name;
+    string comment;
+    Feld *felder;
+    int feldzahl;
+    Index *indices;
+    int indexzahl;
+    const string engine;
+    const string charset;
+    const string collate;
+    const string rowformat;
+    Tabelle(const string& name, Feld *felder, int feldzahl, Index *indices, int vindexzahl, string comment="",
+        const string& engine="InnoDB", const string& charset="latin1", const string& collate="latin1_german2_ci", const string& rowformat="DYNAMIC");
+};
+
+class RS;
+
+class DB 
+{
+  public:
+    // muss außerhalb der Klasse und einer Funktion noch mal definiert werden
+    static uchar oisok; // 1=Installation von MariaDB wurde ueberprueft
+    string db_systemctl_name; // mysql, mariadb je nach System
+    servc *dbsv=0;
+    MYSQL *conn;
+		linst_cl *linstp=0;
+#ifdef mitpostgres 
+		PGconn *pconn,*pmconn;
+#endif
+    ////	MYSQL_RES *result;
+    ////	MYSQL_ROW row;
+    DBSTyp DBS;
+    char dnb; // delimiter name begin
+    char dne; // delimiter name end
+    char dvb; // delimiter value begin
+    char dve; // delimiter value end
+    ////	char* ConStr;
+    ////  const char* db;
+    string mysqlbef="mysql", mysqlben="mysql"; // mysql-Befehl, mysql-Benutzer
+    string db;
+    ////  const char* host;
+    string host;
+    ////  const char* user;
+    string user;
+    string passwd;
+    string myloghost; // einheitliche Benennung von 'localhost' bzw. '%'
+    string rootpwd; // root-Passwort
+    unsigned int fehnr;
+    const char* ConnError;
+    string sql;
+    uchar miterror;
+    vector<string> myr;
+    string cmd;
+    string datadir;
+    RS *spalt=nullptr;
+		char **spnamen=nullptr,**splenge=nullptr,**sptyp=nullptr;
+	private:
+		void instmaria(int obverb, int oblog);
+	public:
+		int usedb(const string& db);
+		void pruefrpw(const string& wofuer, unsigned versuchzahl);
+		void setzrpw(int obverb=0, int oblog=0);
+		void prueffunc(const string& pname, const string& body, const string& para, int obverb, int oblog);
+		my_ulonglong arows;
+		vector< vector<instyp> > ins;
+		void erweitern(const string& tab, vector<instyp> einf,int obverb,uchar obsammeln=0, const unsigned long *maxl=0);
+		uchar tuerweitern(const string& tab, const string& feld,long wlength,int obverb);
+    int machbinaer(const string& tabs, const string& fmeld,int obverb);
+    ////	DB(DBSTyp DBS, const char* host, const char* user,const char* passwd, const char* db, unsigned int port, const char *unix_socket, unsigned long client_flag);
+		DB(linst_cl *linstp);
+		DB(DBSTyp nDBS, linst_cl *linstp, const char* const phost, const char* const user,const char* const ppasswd, const char* const uedb="", 
+		   unsigned int port=0, const char *const unix_socket=NULL, unsigned long client_flag=0,
+			 int obverb=0,int oblog=0,int versuchzahl=3,uchar ggferstellen=1);
+    DB(DBSTyp nDBS, linst_cl *linstp, const char* const phost, const char* const user, const char* const ppasswd, const char* const prootpwd, 
+		   const char* const uedb="", unsigned int port=0, const char *const unix_socket=NULL, unsigned long client_flag=0,
+			 int obverb=0,int oblog=0,int versuchzahl=3,
+       uchar ggferstellen=1);
+    DB(DBSTyp nDBS, linst_cl *linstp, const string& phost, const string& puser, const string& ppasswd, const string& uedb, unsigned int port, 
+		   const char* const unix_socket, unsigned long client_flag=0,
+       int obverb=0,int oblog=0,int versuchzahl=3,uchar ggferstellen=1);
+    void init(DBSTyp nDBS, const char* const phost, const char* const user,const char* const ppasswd, const char* const uedb="",
+				unsigned int port=0, const char *const unix_socket=NULL, unsigned long client_flag=0,int obverb=0,int oblog=0,unsigned versuchzahl=3,
+				uchar ggferstellen=1);
+    ~DB(void);
+    /*//
+       int Abfrage(const char* sql,const char** erg=(const char**)&"", int obverb=1);
+       int Abfrage(string sql,const char** erg=(const char**)&"", int obverb=1);
+       int AbfragemE(const char* sql,const char** erg=(const char**)&"", int obverb=1); // mit Ende
+       int AbfragemE(string sql,const char** erg=(const char**)&"", int obverb=1);      // mit Ende
+     */
+    ////	int insert(const char* tab, vector<instyp> einf, const char** erg=(const char**)&"",uchar sammeln=0);
+    ////	void AbfrageEnde();
+    void LetzteID(string* erg);
+    char* tmtosql(tm *tmh,char* buf);
+    char* tmtosqlmZ(tm *tmh,char* buf);
+    ////	char** HolZeile();
+    my_ulonglong affrows(); // unsigned __int64
+    void lesespalten(Tabelle *tab,int obverb=0,int oblog=0);
+    int prueftab(Tabelle *tab,int obverb=0,int oblog=0);
+		int machind(const string& tname,Index *indx,int obverb=0, int oblog=0);
+};
+
+
+class RS 
+{
+  public:
+    DB* db;
+    string sql;
+    string isql; // insert-sql
+    uchar obfehl;
+    string fehler;
+		char **betroffen=0; // fuer Abfrage in postgres
+    unsigned int fnr;
+    MYSQL_RES *result;
+#ifdef mitpostgres 
+		PGresult *pres;
+#endif
+		unsigned long *lengths;
+    MYSQL_ROW row;
+    unsigned int num_fields;
+    unsigned long long  num_rows;
+    string table;
+    vector<string> feld;
+    vector<string> typ;
+    vector<long> lenge;
+    vector<long> prec;
+    vector<string> kommentar;
+    RS(DB* pdb);
+    char*** HolZeile();
+    void weisezu(DB* pdb);
+    void clear();
+    template<typename sT> 
+      int Abfrage(sT psql,int obverb=1,uchar asy=0,int oblog=0){
+        int erg=-1;
+        this->sql=psql;
+        if (!sql.empty()) {
+          erg = doAbfrage(obverb,asy,oblog);
+        } //         if (!sql.empty())
+        return erg;
+      } //       int Abfrage(sT psql,int obverb=1,uchar asy=0)
+
+    RS(DB* pdb,const char* const psql, int obverb=1);
+    RS(DB* pdb,const string& psql, int obverb=1);
+    RS(DB* pdb,stringstream psqls, int obverb=1);
+    ~RS();
+    void update(const string& tab,vector<instyp> einf,int obverb, const string& bedingung, uchar asy=0);
+    void insert(const string& tab,vector<instyp> einf,uchar sammeln=0,int obverb=1,string *id=0,uchar eindeutig=0,uchar asy=0,
+		            svec *csets=0);
+  private:
+    int doAbfrage(int obverb=0,uchar asy=0,int oblog=0);
+};
+
+////string ersetze(const char *u, const char* alt, const char* neu);
+#endif // DB_H_DRIN
---
+++ vgb.cpp
@@ -0,0 +1,150 @@
+#include "kons.h"
+#include "DB.h"
+#include "autofax.h"
+
+enum Txvg_ {
+  T_VorgbSpeziell_Ueberlad,
+  T_vgMAX,
+};
+
+/*
+class Txvgcl: public TxB
+{
+  public:
+    static const char *TextC[][SprachZahl];
+    Txvgcl() {TCp=(const char* const * const * const *)&TextC;}
+};
+*/
+
+// const char *Txvgcl::TextC[T_vgMAX+1][SprachZahl]={
+const char *vgb_T[T_vgMAX+1][SprachZahl]={
+      // T_VorgbSpeziell_Ueberlad
+      {"VorgbSpeziell_Ueberlad()","specificprefs_overload()"},
+      {"",""}
+}; // const char *Txvgcl::TextC[T_vgMAX+1][SprachZahl]=
+
+// class Txvgcl Txv;
+class TxB Txv((const char* const* const* const*)vgb_T);
+
+void paramcl::VorgbSpeziell()
+{
+  Log(violetts+Txv[T_VorgbSpeziell_Ueberlad]+schwarz);
+  //  langu="d"; lgnzuw();
+  dbq="faxeinp";
+  muser="praxis";
+  citycode="8131";
+  msn="616381";
+  LocalIdentifier="DiabDachau";
+  cFaxUeberschrift="Diabetologie Dachau";
+
+  // wird schon fuer systemrueck benoetigt
+  sqlz=sqlvz="7";
+  const char *hatab = "haerzte_neu",
+        *officetab = "office",
+        *quelltab = "quelle",
+        *kvaetab  = "kvaerzte";
+
+  sqlzn=sqlvzn=atol(sqlvz.c_str());
+  if (sqlvzn) {
+//    sqlconfvp= new cppSchluess[sqlvzn];
+    sqlconfv.neu(sqlvzn);
+    for(size_t i=0;i<sqlvzn;i++) {
+      sqlconfv[i].name=string("SQL_")+ltoan(i);
+      switch (i) {
+        case 0:
+          sqlconfv[i].wert = string("")+"select if(isnull(arzt),bsname,arzt) getName,bsname from (select bsname, FaxZahl, "
+            "concat(group_concat(distinct Arzt separator ', '), ', ',ort,';') Arzt "
+            "from (SELECT bs.name bsname, f.faxzahl, concat(if(instr(Titel,'.')<>0,concat(left(titel,instr(titel,'.')),' '),Titel), "
+            "left(a.Vorname,1), '.', a.Nachname, if(count(a.nachname)>6,'',concat(' (',group_concat(distinct "
+            "replace(replace(replace(replace(replace(replace(replace(replace(replace(replace(replace(replace(replace(replace(replace(replace(replace"
+            "(replace(replace(replace(replace(replace(replace(replace(replace(replace(if(isnull(fachrichtung),'',fachrichtung),"
+            "'Facharzt für ','FA f.'),'Fachärztin für ','FÄ f.'),'Schwerpunkt',''),'edizin','ed.'),'Allgemeinmed.','Allgm.'),"
+            "'therapeutische','th.'),'ologie','ol.'),'therapeutin','th.'),'therapeut','th.'),'heilkunde','hk.'),'therapie','th.'),"
+            "'irurgie','ir.'),'pädie','p.'),'Jugendlichen','Jugdl.'),'Innere ','In.'),'krankheiten','krht.'),'hilfe','h.'),"
+            "'Praktischer Arzt','Prakt.A.'),'Praktische Ärztin','Prakt.Ä.'),'Diagnostische ','diagn.'),'ologe','ol.'),"
+            "'Hals-Nasen-Ohrenhk.','HNO'),'ologische ','ol.'),'ologischer ','ol.'),'istische','ist.'),'und ','u.') separator ', '),')'))) Arzt, "
+            "ort.Ort "
+            "from `"+hatab+"`.fax f left join `"+hatab+"`.arzt_has_bs ahb on f.bs_id = ahb.bs_id "
+            "left join `"+hatab+"`.bs on f.bs_id = bs.idbs "
+            "left join `"+hatab+"`.arzt a on ahb.arzt_id = a.idarzt "
+            "left join `"+hatab+"`.arzt_has_fachrichtung ahf on ahb.arzt_id = ahf.arzt_id "
+            "left join `"+hatab+"`.fachrichtung fr on ahf.fachrichtung_id = fr.idfachrichtung "
+            "left join `"+hatab+"`.ort on bs.ort_id = ort.idort "
+            "left join `"+hatab+"`.titel t on a.titel_id = t.idtitel "
+						"where concat(if(instr('12345678',mid(faxzahl,1,1)) and not (length(faxzahl)>8),'08131',''),"
+            "replace(replace(replace(replace(faxzahl,' ',''),'-',''),'/',''),'\\'','')) = '&&faxnr&&' "
+            "group by faxzahl, nachname, vorname) i) i group by FaxZahl";
+          break;
+        case 1:
+          sqlconfv[i].wert = string("")+"select concat(titel,if(titel='','',' '),vorname,' ',nvorsatz,if(nvorsatz='','',' '),nachname,', g.',"
+            "date_format(gebdat,'%d.%m.%y'),', ',ort) name,concat('Pid ',pat_id) bsname from "+quelltab+".namen n where "
+            "concat(if(instr('12345678',mid(privatfax,1,1)) and not (length(privatfax)>8),'08131',''),"
+						"replace(replace(replace(replace(privatfax,' ',''),'-',''),'/',''),"
+            "'\\'','')) = '&&faxnr&&'";;
+          break;
+        case 2:
+          sqlconfv[i].wert = string("")+"select concat(haname,', ',ort,', ',kvnu) name, zulg,fax1k from "+kvaetab+".hae where "
+            "concat(if(instr('12345678',mid(fax1k,1,1)) and not (length(fax1k)>8),'08131',''),"
+						"replace(replace(replace(replace(fax1k,' ',''),'-',''),'/',''),'\\'','')) = " 
+            "'&&faxnr&&'";
+          break;
+        case 3:
+          sqlconfv[i].wert = string("")+"select concat(titel,if(titel='','',' '),vorname,' ',name,', ',ort) name, zusatzfeld5 from "+
+            officetab+".adresse where concat(if(instr('12345678',mid(faxk,1,1)) and not (length(faxk)>8),'08131',''),"
+						"replace(replace(replace(replace(faxk,' ',''),'-',''),'/',''),'\\'',''))"
+            " = '&&faxnr&&' order by zusatzfeld5 desc";
+          break;
+        case 4:
+          sqlconfv[i].wert = string("")+"SELECT concat(if(isnull(name),'',concat(titel,if(titel='','',' '),vorname,' ',name,', ',ort, ', ')),"
+            "if(isnull(i.firma),'',i.firma)) name, i.abteilung FROM "+officetab+".telefon t left join "+officetab+".kontakte k "
+            "on t.knr  = k.knr left join "+officetab+".pers p on k.knr = p.knr left join "+officetab+".inst i on k.knr = i.knr left join "+
+            officetab+".anschr a on a.knr = p.knr where concat(if(instr('12345678',mid(num,1,1)) and not (length(num)>8),'08131',''),"
+            "replace(replace(replace(replace(num,' ',''),'-',''),'/',''),'\\'',''))  = '&&faxnr&&' and (not isnull(name) "
+            "or not isnull(firma) or not isnull(vorname)) order by name desc, vorname desc, firma desc";
+          break;
+        case 5:
+          sqlconfv[i].wert = string("")+"select concat(titel,if(titel='','',' '),vorname,' ',nvorsatz,if(nvorsatz='','',' '),"
+            "nachname,', g.',date_format(gebdat,'%d.%m.%y'),', ',ort) name,concat('Pid ',pat_id) bsname from "+quelltab+".namen n where "
+            "concat(if(instr('12345678',mid(privattel,1,1)) and not (length(privattel)>8),'08131',''),"
+						"replace(replace(replace(replace(privattel,' ',''),'-',''),'/',''),'\\'','')) = "
+            "'&&faxnr&&'";
+          break;
+        case 6:
+          sqlconfv[i].wert = string("")+"select concat(titel,if(titel='','',' '),vorname,' ',name) name,fachgruppe from "+quelltab+
+            ".listenausgabeuew where concat(if(instr('12345678',mid(fax,1,1)) and not (length(fax)>8),'08131',''),"
+						"replace(replace(replace(replace(fax,' ',''),'-',''),'/',''),'\\'','')) "
+            "= '&&faxnr&&'";
+          break;
+      }
+    }
+  }
+  // cpt in VorgbAllg festgelegt
+  if (strstr(cpt,"linux")){
+//  host="localhost"; // 'localhost' schon Vorgabe bei Definition
+    if (!strcmp(cpt,"linux2")) {
+      zufaxenvz="/vista/P/zufaxen"; 
+      wvz="/vista/P/warteauffax";
+      nvz="/vista/P/nichtgefaxt";
+      empfvz="/vista/P";
+    } else {
+      zufaxenvz="/DATA/Patientendokumente/zufaxen";
+      wvz="/DATA/Patientendokumente/warteauffax";
+      nvz="/DATA/Patientendokumente/nichtgefaxt";
+      empfvz="/DATA/Patientendokumente";
+    }
+  } else {
+  }
+  if (strstr(cpt,"linux")){
+    if (!strcmp(cpt,"linux2")) {
+      static zielmustercl zmi[]={zielmustercl("[Aa]rztbrief","/vista/P"),zielmustercl("","/vista/P/gefaxt")};
+      zmvp=zmi;
+      zmvzn=sizeof zmi/sizeof *zmi;
+    } else { // linux1
+      static zielmustercl zmi[]={zielmustercl("[Aa]rztbrief","/DATA/Patientendokumente"),zielmustercl("","/DATA/Patientendokumente/gefaxt")};
+      zmvp=zmi;
+      zmvzn=sizeof zmi/sizeof *zmi;
+    }
+  } else {
+  }
+} // void paramcl::VorgbSpeziell()
+
